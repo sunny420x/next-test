4c7881ba32548d23012e1e012f30dd10d91da214
Sunny420x <costerraid@gmail.c.com>
2025-03-04 12:40:41 +0700

    styling update

--- src/app/our-team/[slug]/page.tsx
+++ src/app/our-team/[slug]/page.tsx
@@ -84,7 +84,7 @@ export default async function TeamMemberDetail({
             <h3 className="text-base/7 font-semibold text-gray-900">Team Member Information</h3>
             <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">Personal details and application.</p>
         </div>
-        <div className="mt-6 border-t border-gray-100">
+        <div className="border-t border-gray-100">
             <dl className="divide-y divide-gray-100">
                 <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
                     <dt className="text-sm/6 font-medium text-gray-900">ชื่อ</dt>
@@ -94,9 +94,10 @@ export default async function TeamMemberDetail({
                 </div>
             </dl>
         </div>
-      {teamMember.blocks.map((block: TeamPageBlock) => (
-        <BlockRenderer key={block.id} block={block} />
-      ))}
+        <hr/>
+        {teamMember.blocks.map((block: TeamPageBlock) => (
+            <BlockRenderer key={block.id} block={block} />
+        ))}
     </main>
   );
 }
