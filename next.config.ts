9c217a329931ad7a4bd36648126646a655bf0b7d
Sunny420x <costerraid@gmail.c.com>
2025-03-06 16:50:00 +0700

    next config update

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     domains: ['localhost', 'strapi-qxa4.onrender.com'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "strapi-qxa4.onrender.com",
+        port: "",
+        pathname: "/uploads/**/*",
+      }
+    ]
   },
 };
 
